f66f1ecc60daa44f30f9568a265bffdc36a1924e
mohanprasaths <mohanprasath@live.com>
2017-05-31 15:13:46 +0530

    MultipleLinear Regression

--- MultipleLinearRegression/multiple_linear_regress.py
+++ MultipleLinearRegression/multiple_linear_regress.py
@@ -7,11 +7,25 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Startups.csv')
 X = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,1].values
+y = dataset.iloc[:,4].values
         
+
+
+# Encoding categorical data
+# Encoding the Independent Variable
+# Converting the label "string" values to number and make them categorical
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Remove dummy variable trap
+X = X[: , 1:]
+
 #Split training and test data               
 from sklearn.cross_validation import train_test_split
-X_test, X_train, y_test, y_train,  = train_test_split(X,y,train_size= 1/3,random_state=0)
+X_train, X_test, y_train, y_test,  = train_test_split(X,y,test_size= 0.2,random_state=0)
 
 #Fit the regressor and use the mean and standard deviation in Prediction
 from sklearn.linear_model import LinearRegression
